1338cc6ad626e019da22e0013dd24e3aee09019e
Connor Mallon <cmallonisme@gmail.com>
2025-03-13 16:48:05 +0000

    Adding logs - not completely working through sharding

--- Blockchain_Codebase/blockchain-visualizer/src/app/page.tsx
+++ Blockchain_Codebase/blockchain-visualizer/src/app/page.tsx
@@ -1,12 +1,18 @@
 "use client"; // Ensures this runs as a client-side component
 
 import BlockchainInteract from "./Blockchain_Interact";
+import ExecutionPanel from "./ExecutionPanel";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-8 pb-20 sm:p-20 flex flex-col items-center">
-      <h1 className="text-3xl font-bold text-center mb-6"> </h1>
-      
+    <div className="min-h-screen p-8 pb-20 sm:p-20 flex-col items-center">
+      <h1 className="text-3xl font-bold text-center mb-6">
+        Spider-Web Blockchain View
+      </h1>
+
+      {/* Add ExecutionPanel ABOVE the Blockchain Visualization */}
+      <ExecutionPanel />
+
       {/* Blockchain Visualization Component */}
       <BlockchainInteract />
     </div>
